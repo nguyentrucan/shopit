4b709a54c7c2e26877a1a4d10cfc340460139c86
Nguyễn Trúc An <114329768+nguyentrucan@users.noreply.github.com>
2023-11-13 15:26:06 +0700

    update resetPassword & Handle Wrong JWT Token & Expire JWT Error

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -28,7 +28,25 @@ module.exports = (err, req, res, next) => {
             const message = Object.values(err.errors).map(value => value.message);
             error = new ErrorHandler(message, 400)
         }
-        
+
+        //Handling Mongoose duplicate key errors
+        if (err.code === 11000) {
+            const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+            error = new ErrorHandler(message, 400)
+        }
+
+        //Handling Wrong JWT error
+        if (err.name === 'JsonWebTokenError') {
+            const message = 'JSON Web Token is invalid. Try Again !';
+            error = new ErrorHandler(message, 400)
+        }
+
+        //Handling Expired JWT error
+        if (err.name === 'TokenExpiredError') {
+            const message = 'JSON Web Token is expired. Try Again !';
+            error = new ErrorHandler(message, 400)
+        }
+
         res.status(error.statusCode).json({
             success: false,
             message: error.message || 'Internal Server Error'
